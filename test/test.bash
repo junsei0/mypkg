efd3a1eec1869a26f40fd33a765180a87414ad2d
Junsei Iimori <moririn638@gmail.com>
2025-01-04 18:09:50 +0900

    update test

--- test/test.bash
+++ test/test.bash
@@ -6,7 +6,56 @@ dir=~
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
-timeout 10 ros2 launch maypkg talk_listen.launch.py > /tmp/mypkg.log
+timeout 10 ros2 launch maypkg talk_listen.launch.py > /tmp/mypkg.log &
 
-cat /tmp/mypkg.log |
-grep 'Listen: 10'
+sleep 10
+
+previous_time=0
+landmark_list=("エッフェル塔: 緯度 48.8583, 経度 2.2944" \ 
+               "自由の女神: 緯度 40.6892, 経度 -74.0445" \ 
+               "東京タワー: 緯度 35.6586, 経度 139.7454" \ 
+               "シドニー・オペラハウス: 緯度 -33.8568, 経度 151.2153" \                
+               "ギザのピラミッド: 緯度 29.5845, 経度 31.0803")
+landmark_counter=0
+interval_check=true
+order_check=true
+
+while read -r line; do
+    if [[ "$line" =~ "Publishing: " ]]; then
+        message=$(echo "$line" | sed 's/.*Publishing: "\(.*\)"/\1/')
+
+        if [ "$message" == "${landmark_list[$landmark_counter]}" ]; then
+            landmark_counter=$(( (landmrak_counter + 1) % ${#landmark_list[@]} ))
+        else
+            order_check=false
+            break
+        fi
+
+        timestamp=$(echo "$line" | grep -op '\[\d+\.\d+\]' | tr -d '[]')
+        timestamp_int=${timestamp%.*}
+
+        if [ $previous_time -eq 0]; then
+            previous_time=$timestamp_int
+            continue
+        fi
+
+        interval=$((timestamp_int - previous_time))
+        if [ $interval -ne 2 ]; then
+            interval_check=false
+            break
+        fi
+
+        previous_time=$timestamp_int
+    fi
+done < /tmp/landmark_test.log
+
+if [ "$order_check" = true ] && [ "$interval_check" = true ]; then
+    echo "success"
+else
+    if [ "$order_check" = false ]; then
+        echo "number false"
+    fi
+    if [ "$interval_check" = false ]; then
+        echo "sending interval false"
+    fi
+fi
